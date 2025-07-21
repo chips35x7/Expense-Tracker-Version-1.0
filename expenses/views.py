8852c3c08c103f0766caf5970bbeeff67f783fbb
Chips3507 <niggschiputura@gmail.com>
2025-07-21 09:03:14 +0200

    Added custom permissions for viewing expenses and updated the views

--- expenses/views.py
+++ expenses/views.py
@@ -2,16 +2,19 @@ from rest_framework import generics
 
 from .models import Expense
 from .serializers import ExpenseSerializer
+from .permissions import IsOwnerOrRestrictAccess
 
 class ExpenseList(generics.ListCreateAPIView):
     serializer_class = ExpenseSerializer
-    queryset = Expense.objects.all()
 
     def perform_create(self, serializer):
-        # return super().perform_create(serializer)
         serializer.save(user=self.request.user)
 
+    def get_queryset(self):
+        queryset = Expense.objects.filter(user=self.request.user)
+        return queryset
 
 class ExpenseDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrRestrictAccess,)
     serializer_class = ExpenseSerializer
     queryset = Expense.objects.all()
