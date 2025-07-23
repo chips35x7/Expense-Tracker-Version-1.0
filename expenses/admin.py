30b8e4eb21f454052a3b2d2e813adfecf81e959a
Chips3507 <niggschiputura@gmail.com>
2025-07-23 07:58:29 +0200

    Updated the Expense model and customized the admin

--- expenses/admin.py
+++ expenses/admin.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 
 from .models import Expense
 
+
+@admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = [
-        'user',
-        'category',
-        'amount',
-        'date_created'
-    ]
-
-
-admin.site.register(Expense, ExpenseAdmin)
+    list_display = ['user', 'category', 'amount', 'created', 'updated']
+    list_filter = ['category', 'created', 'updated']
+    date_hierarchy = 'created'
+    search_fields = ['category']
+    ordering = ['-created']
+    show_facets = admin.ShowFacets.ALWAYS
--- expenses/models.py
+++ expenses/models.py
@@ -3,34 +3,42 @@ from django.conf import settings
 
 import uuid
 
-expense_categories = {
-    'Food': 'Food',
-    'Transport': 'Transport',
-    'Utilities': 'Utilities',
-    'Housing': 'Housing',
-    'Health': 'Health',
-    'Entertainment': 'Entertainment',
-    'Education': 'Education',
-    'Shopping': 'Shopping',
-    'Savings': 'Savings',
-    'Debt': 'Debt',
-    'Other': 'Other',
-}
 
 class Expense(models.Model):
+    class ExpenseCategories(models.TextChoices):
+        FOOD = 'FD', 'Food'
+        TRANSPORT = 'TRNSPT', 'Transport'
+        UTILITIES = 'UTLTY', 'Utilities'
+        HOUSING = 'HSING', 'Housing'
+        HEALTH = 'HLTH', 'Health'
+        ENTERTAINMENT = 'ENTTNMNT', 'Entertainment'
+        EDUCATION = 'EDCTION', 'Education'
+        SHOPPING = 'SHP', 'Shopping'
+        SAVINGS = 'SVING', 'Savings'
+        DEBT = 'DBT', 'Debt'
+        OTHER = 'OTHR', 'Other'
+
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False
     )
-    category = models.CharField(max_length=30, choices=expense_categories)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date_created = models.DateTimeField(auto_now_add=True)
-    description = models.CharField(max_length=300, blank=True, null=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='expenses'
     )
+    category = models.CharField(max_length=15, choices=ExpenseCategories)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=250, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['created'])
+                ]
+    
     def __str__(self):
         return self.category
