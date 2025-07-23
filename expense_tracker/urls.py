2a66850106551a473c01db983129a573552320fa
Chips3507 <niggschiputura@gmail.com>
2025-07-23 09:56:24 +0200

    Added documentation (Redoc & SwaggerUI)

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -44,7 +44,8 @@ urlpatterns = [
 
     # Schema and documentation endpoints
     path('api/schema', SpectacularAPIView.as_view(), name='schema'),
-    
+    path('api/schema/redoc', SpectacularRedocView.as_view(url_name="schema"), name='redoc'),
+    path('api/schema/swagger-ui', SpectacularSwaggerView.as_view(url_name="schema"), name='swagger-ui'),
 ]
 
 # Using browser based session authentication in development 
